c32c26594e182b42d32d123ef3d1eba095a16b8f
Admin <Admin@DESKTOP-LKF6Q46>
2023-04-12 14:47:48 +0800

    ariunmaa bischsen shuu

--- src/aaa/anuka.java
+++ src/aaa/anuka.java
@@ -3,8 +3,12 @@ package aaa;
 public class anuka {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+		      int[] myNumbers = {1, 2, 3};
+		      System.out.println(myNumbers[10]);
+		    } catch (Exception e) {
+		      System.out.println("arimunaa bichsen.");
+		    }
 	}
 
 }
